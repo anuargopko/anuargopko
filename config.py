ef921fbdee8fc1857c803b31cd6f7caee66848ba
anuargopko <84816547+anuargopko@users.noreply.github.com>
2021-05-26 03:41:25 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+API_TOKEN = '1892817410:AAEtX4ZZRltHWgeeGxWUacmXOlh_f7ZPTqw' # Токен бота, получать у @BotFather
+QIWI_ACCOUNT = '77077000994' # Номер телефона киви кошелька (71111111111)
+BTC_ADDRESS = '1UsGkjUX87AnoEwv4fARy7kfEkbfdzcPq' # Номер btc адреса на которую будут поступать средства
+CARD_NUMBER = '5599 0050 8409 8248' # Номер банк. карты на которую будут поступать средства
